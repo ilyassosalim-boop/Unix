2273ee8b13f9a04d507879577796abec876d4e3d
ilyassosalim-boop <ilyassosalim@gmail.com>
2026-01-18 11:29:34 +0100

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,24 +1,53 @@
-#!/usr/bin/env bash
-# File: guessinggame.sh
+#!/bin/bash
 
-function guessinggame(){
-    no_of_files=$(pwd | ls | wc -l)
-    while true;
-    do
-        echo "pleas enter your guess"
-        read  number
-        if [ $number -lt $no_of_files ]
-        then
-            echo "Your guess is less than the number of files"
-        elif [ $number -gt $no_of_files ]
-        then
-            echo "Your guess is greater than the number of files"
-        else
-            echo "Congratulation,your guess is correct!"
-        break;
+# Function to count files in current directory (excluding directories)
+count_files() {
+    local file_count=$(ls -l | grep '^-' | wc -l)
+    echo $file_count
+}
+
+# Function to compare guess with actual count
+check_guess() {
+    local guess=$1
+    local actual=$2
+    
+    if [[ $guess -lt $actual ]]; then
+        echo "Too low!"
+        return 1
+    elif [[ $guess -gt $actual ]]; then
+        echo "Too high!"
+        return 1
+    else
+        echo "Congratulations! You guessed the correct number of files."
+        return 0
+    fi
+}
+
+# Main game function
+play_game() {
+    local actual_count=$(count_files)
+    local user_guess
+    local game_over=0
+    
+    echo "Welcome to the Guessing Game!"
+    echo "=============================="
+    
+    while [[ $game_over -eq 0 ]]; do
+        echo "How many files are in the current directory?"
+        read -p "Enter your guess: " user_guess
+        
+        # Validate input is a number
+        if ! [[ "$user_guess" =~ ^[0-9]+$ ]]; then
+            echo "Please enter a valid number."
+            continue
+        fi
+        
+        check_guess $user_guess $actual_count
+        if [[ $? -eq 0 ]]; then
+            game_over=1
         fi
     done
 }
 
-echo "Guess how many files are there in current directory?"
-guessinggame
+# Start the game
+play_game
